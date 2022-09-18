29e51e715f98ddbf4ad7fae75c5274b8871ca505
Mamadou Sarr <mbayelel@gmail.com>
2022-09-18 02:16:00 +0000

    calcul moyenne with rust

--- src/input.rs
+++ src/input.rs
@@ -1,8 +1,36 @@
-pub fn get_line(mut line: String) -> String {
+use std::{
+    fmt::{Debug, Display},
+    str::FromStr,
+};
+
+pub fn get_line() -> String {
+    let mut line = String::new();
     let b1 = std::io::stdin().read_line(&mut line).unwrap();
     println!("have read , {} bytes", b1);
 
     return line;
+}
+
+pub fn trim_newline(mut line: String) -> String {
+    if line.ends_with("\n") {
+        line.pop();
+    }
+    if line.ends_with("\r") {
+        line.pop();
+    }
+    return line;
+}
+
+pub struct E;
+
+pub fn parse_input<T: FromStr + Display + Debug>(line: String) -> Result<T, E> {
+    match trim_newline(line).parse::<T>() {
+        Ok(i) => {
+            let n = i;
+            return Ok(n);
+        }
+        Err(_) => Err(E),
+    }
 }
 
 #[cfg(test)]
@@ -11,10 +39,20 @@ mod tests {
 
     #[test]
     fn test_get_line() {
-        let mut line = String::new();
-        println!("Entrez votre nom: ");
-        line = get_line(line);
+        let line = get_line();
         assert_eq!(line, "sarr\n");
         assert_ne!(line, "sarr");
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let a = parse_input::<f64>(String::from("2")).unwrap_or(0.0);
+        let b = parse_input::<f64>(String::from("2")).unwrap_or(0.0);
+        let c = a / b;
+        assert_eq!(c, 1.0);
+        assert_eq!(
+            parse_input::<f64>(String::from("12.5")).unwrap_or(0.00),
+            12.5
+        );
     }
 }
--- src/moyenne.rs
+++ src/moyenne.rs
@@ -1,8 +1,21 @@
-use crate::input;
+use crate::input::get_line;
+use crate::input::parse_input;
 
 pub fn calc_moy() {
-    let mut nom = String::new();
-    println!("Entrez votre nom");
-    nom = input::get_line(nom);
-    println!("Votre nom est {}", nom);
+    println!("Entrez votre nom: ");
+    let nom = get_line();
+    println!("Entrez votre prenom: ");
+    let prenom = get_line();
+    println!("Entrez la premiere note");
+    let note1: f64 = parse_input::<f64>(get_line()).unwrap_or(0.00);
+    println!("Entrez la deuxième note");
+    let note2: f64 = parse_input::<f64>(get_line()).unwrap_or(0.00);
+    let moy: f64 = (note1 + note2) / 2.0;
+
+    println!(
+        "Bonjour {prenom} {nom} votre moyenne est {moy}",
+        prenom = prenom,
+        nom = nom,
+        moy = moy
+    );
 }
